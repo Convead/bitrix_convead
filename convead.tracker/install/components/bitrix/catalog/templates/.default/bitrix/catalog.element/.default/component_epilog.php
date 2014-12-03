349a2e6f657aab8d1e0f9f7b3c518407031c42e9
Alexey <alexey@milovidov.me>
2014-12-03 11:30:44 +0300

    Garbage cleansing

--- convead.tracker/install/components/bitrix/catalog/templates/.default/bitrix/catalog.element/.default/component_epilog.php
+++
@@ -1,7 +0,0 @@
-<?
-if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true) die();
-
-if(CModule::IncludeModule('convead.tracker'))
-	cConveadTracker::productView($arResult, CUser::GetID());
-
-?>
--- platina.tracker/install/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-$arModuleVersion = array(
-    "VERSION" => "1.0.0",
-    "VERSION_DATE" => "2014-10-01 15:00:00"
-);
-?>
