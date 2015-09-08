d563899f19372065f6b1858a6e0a5c6ab681b25c
Saveljev Vladimir <v.saveljev@secoint.ru>
2015-09-08 11:52:45 +0300

    Go to SSL. Version to 1.1.8

--- platina.conveadtracker/install/version.php
+++ platina.conveadtracker/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.1.7",
-    "VERSION_DATE" => "2015-08-24 16:00:00"
+    "VERSION" => "1.1.8",
+    "VERSION_DATE" => "2015-09-08 11:00:00"
 );
 ?>
