4d317cefad58f7c7087e9f733068ef79c7dd4cff
Saveljev Vladimir <v.saveljev@secoint.ru>
2015-09-15 14:33:10 +0400

    Https widget. Version to 1.1.9

--- platina.conveadtracker/install/version.php
+++ platina.conveadtracker/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.1.8",
-    "VERSION_DATE" => "2015-09-08 11:00:00"
+    "VERSION" => "1.1.9",
+    "VERSION_DATE" => "2015-09-15 14:00:00"
 );
 ?>
