71a26166702edb46fbbb06ab55c37dc345049059
Saveljev Vladimir <v.saveljev@secoint.ru>
2015-11-27 10:16:22 +0300

    Bump version to 1.2.3

--- platina.conveadtracker/install/version.php
+++ platina.conveadtracker/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.2.2",
-    "VERSION_DATE" => "2015-11-25 11:00:00"
+    "VERSION" => "1.2.3",
+    "VERSION_DATE" => "2015-11-27 10:15:00"
 );
 ?>
