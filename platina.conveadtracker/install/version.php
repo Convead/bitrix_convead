f7185a1fbb7f9b25bf77428efb4c276250678dd0
Vladimir Saveliev <vova@convead.com>
2015-11-30 13:52:15 +0300

    Bump version to 1.2.4

--- platina.conveadtracker/install/version.php
+++ platina.conveadtracker/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.2.3",
-    "VERSION_DATE" => "2015-11-27 10:15:00"
+    "VERSION" => "1.2.4",
+    "VERSION_DATE" => "2015-11-30 14:00:00"
 );
 ?>
