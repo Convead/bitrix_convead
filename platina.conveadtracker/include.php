e4da00adffa260e64bb3b85a8b4ccdc7087e9bcb
Saveljev Vladimir <v.saveljev@secoint.ru>
2016-10-11 10:41:02 +0300

    Refactoring

--- platina.conveadtracker/include.php
+++ platina.conveadtracker/include.php
@@ -1,13 +1,13 @@
 <?php
 CModule::IncludeModule("platina.conveadtracker");
 
-$arClasses=array(
+$arClasses = array(
   'ConveadTracker'=>'classes/ConveadTracker.php',
   'ConveadApi'=>'classes/ConveadTracker.php',
   'ConveadBrowser'=>'classes/ConveadTracker.php',
   'cConveadTracker'=>'classes/cConveadTracker.php',
 );
 
-CModule::AddAutoloadClasses("platina.conveadtracker",$arClasses);
+CModule::AddAutoloadClasses("platina.conveadtracker", $arClasses);
 
 ?>
