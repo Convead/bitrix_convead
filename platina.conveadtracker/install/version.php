ea421c2a8bf5dadd37adeb4fbf1c1d42cdf47c64
Saveljev Vladimir <v.saveljev@secoint.ru>
2017-07-27 10:45:55 +0300

    Bump version to 1.3.7

--- platina.conveadtracker/install/version.php
+++ platina.conveadtracker/install/version.php
@@ -1,6 +1,6 @@
 <?php
 $arModuleVersion = array(
-    "VERSION" => "1.3.6",
-    "VERSION_DATE" => "2017-07-17 10:00:00"
+    "VERSION" => "1.3.7",
+    "VERSION_DATE" => "2017-07-27 10:00:00"
 );
 ?>
