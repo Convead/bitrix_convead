086f7eb2298c129c5a6ebcf06305808f9e8a498b
Vladimir Savelyev <rostber@yandex.ru>
2019-07-26 09:25:30 +0300

    Get domain without port

--- platina.conveadtracker/classes/cConveadTracker.php
+++ platina.conveadtracker/classes/cConveadTracker.php
@@ -443,7 +443,9 @@ class cConveadTracker {
   }
 
   private static function getDomain() {
-    return !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+    // Without port
+    $domain = !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
+    return array_shift(explode(':', $domain));
   }
 
   private static function getDetailPageUrl($page) {
